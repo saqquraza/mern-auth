9067fc83f637e18b7fc8e1cc6638ef028bfaa50e
Saqlain <saqlainaman0@gmail.com>
2023-10-12 10:56:12 +0530

    Set up the react router dom and route in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import Signin from "./pages/Signin"
+import Profile from "./pages/Profile"
 
 function App() {
   return (
-    <div className='text-red-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/sign-in" element={<Signin/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
