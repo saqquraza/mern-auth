db6fd2f2e0a85ba55d93bbe0e4738d9e39ce540e
Saqlain <saqlainaman0@gmail.com>
2023-10-12 11:17:10 +0530

    created a header component and used in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import About from "./pages/About"
 import SignUp from "./pages/SignUp"
 import Signin from "./pages/Signin"
 import Profile from "./pages/Profile"
+import Header from "./components/Header"
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
@@ -15,7 +17,6 @@ function App() {
         <Route path="/sign-in" element={<Signin/>}/>
         <Route path="/profile" element={<Profile/>}/>
       </Routes>
-
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <div className='bg-slate-200'>
+            <div className='flex justify-between items-center max-w-6xl mx-auto p-2'>
+                <Link to={'/'}>
+                    <h1 className='font-bold'>Auth App</h1>
+                </Link>
+                <ul className='flex gap-3'>
+                    <Link to={'/'}>
+                        <li>Home</li>
+                    </Link>
+                    <Link to={'/about'}>
+                        <li>About</li>
+                    </Link>
+                    <Link to={'/sign-in'}>
+                        <li>Sign In</li>
+                    </Link>
+                    <Link to={'/sign-up'}>
+                        <li>Sign Up</li>
+                    </Link>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header
