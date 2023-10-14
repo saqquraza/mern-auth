2df35ab1c16f2d26dfb019a7d6fe77b235c59f9c
Saqlain <saqlainaman0@gmail.com>
2023-10-14 13:04:06 +0530

    install redux toolkit , react redux and persistredux . created a store , wrapped the app with store and persist , used store and reducer and persist tp store the user data in local storage

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-
+import { signInFailure, signInStart, signInSuccess } from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 function Signin() {
   const [formData, setFormData] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [respMessage, setRespMessage] = useState();
+  const { loading, error } = useSelector((state) => state.user)
   const [isResp, setIsResp] = useState(false);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setIsResp(false)
@@ -17,7 +18,7 @@ function Signin() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true)
+    dispatch(signInStart())
     setIsResp(false)
     const res = await fetch('/api/auth/signin', {
       method: 'POST',
@@ -28,13 +29,12 @@ function Signin() {
     })
     const data = await res.json();
     setIsResp(true)
-    setRespMessage(data?.message)
     if (data?.success === false) {
-      setRespMessage(data?.error);
-      setLoading(false);
+      dispatch(signInFailure(data))
       return;
-    } 
-    setLoading(false);
+    }
+    dispatch(signInSuccess(data))
+    // console.log(dispatch(signInSuccess(data)))
     setFormData({});
     navigate('/')
   }
@@ -45,7 +45,7 @@ function Signin() {
         <input type='email' id='email' placeholder='Enter User Email' className='bg-slate-100 p-2 rounded-lg ' onChange={handleChange} required={true} value={formData.email || ''} />
         <input type='password' id='password' placeholder='Enter User Password' className='bg-slate-100 p-2 rounded-lg ' onChange={handleChange} required={true} value={formData.password || ''} />
         <button disabled={loading} className='bg-slate-600 text-white p-3 rounded-lg uppercase font-semibold hover:opacity-95 disabled:opacity-80'>{loading ? 'loading..' : 'Sign In'}</button>
-        <p className='text-red-500 text-center text-lg'>{isResp && respMessage}</p>
+        <p className='text-red-500 text-center text-lg'>{isResp && error.error}</p>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont Have an account?</p>
